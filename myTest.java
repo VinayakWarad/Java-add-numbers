66103ec755010cc8e492f5b1376f5fc88b8d887d
VinayakWarad <54053743+vinayakwarad@users.noreply.github.com>
2024-12-27 21:20:03 +0530

    Git concepts refresh

---
+++ myTest.java
@@ -0,0 +1,43 @@
+//********************************************************************
+// File: myTest.java 
+// Author: vinayak warad
+// Purpose: To demonstrate how three inputted numbers are added and then
+// displayed in the Java programming language.
+//
+//********************************************************************
+
+import java.util.Scanner;
+public class myTest
+{
+  public static void main(String[] args)
+  {
+  
+    Scanner scan = new Scanner(System.in);
+    int A;
+    int B;
+    int C;
+    int D;
+    
+    System.out.println("Please enter 4 numbers: ");
+    A = scan.nextInt();
+    B = scan.nextInt();
+    C = scan.nextInt();
+    D = scan.nextInt();
+    
+    int add = getData(A, B, C, D);
+    
+    System.out.println("The total sum is: " + add);
+    
+        
+  }
+  
+  public static int getData(int num1, int num2, int num3,int num4)
+  {
+    int total;
+    
+    total = num1 + num2 + num3+num4;
+    
+    return total;
+  }
+    
+}
